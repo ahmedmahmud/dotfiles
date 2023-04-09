59f556f0c7d3a7ee0dcfd97d748ae8445c80e51b
polycrylate <ahmedmahmud12971@gmail.com>
2023-04-09 13:23:31 +0100

    [feat]: 🖊 Add more keybinds to hyprland, add window rules, change border colors, clean up config

--- dotfiles/hypr/setup_scratch.sh
+++ dotfiles/hypr/setup_scratch.sh
@@ -1,8 +1,4 @@
-# alacritty &
-# sleep 0.4
-# hyprctl dispatch movetoworkspace special,Alacritty
 #! /bin/bash
-alacritty &
-sleep 5
-alacritty &
-# hyprctl keyword windowrule "workspace unset,Alacritty"
+wezterm &
+sleep 0.4
+hyprctl dispatch movetoworkspace special,wezterm
